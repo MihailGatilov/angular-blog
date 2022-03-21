3c9b7e41478aaa9f20300229461f5b9c5970cdc7
Гатилов Михаил Александрович <mikhaill.gatilov@constructor.ru>
2022-03-21 11:49:51 +0300

    Приложение "angular-blog".

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyDAxazhVEvcvlO4Ly8q3Qp8JMiegngEdTM',
+  fbDbUrl: 'https://angular-blog-e3546-default-rtdb.firebaseio.com'
+};
